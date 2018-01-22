ae1a0f17012e673ce5e5bfcc211d1f24ee310c95
Jean-Baptiste Lecoin <jb712rasta@gmail.com>
2018-01-22 08:26:56 +0100

    Initial commit

---
+++ src/web/MyServlet.java
@@ -0,0 +1,24 @@
+package web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.*;
+
+@WebServlet("/Hellow")
+public class MyServlet extends HttpServlet {
+
+	public void doGet( HttpServletRequest request, HttpServletResponse response) {
+		try {
+			response.setContentType("text/html");
+			PrintWriter writer=response.getWriter();
+			writer.println("<html>");
+			writer.println("\t<head><title>DummyPage</title></head>");
+			writer.println("\t<body>Agreat(almost)emptypage</body>");
+			writer.println("</html>");
+			writer.close();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+	}
+
+}
